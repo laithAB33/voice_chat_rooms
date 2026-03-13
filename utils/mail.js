6cac6cf5508c426223674d77584dda7b05025aec
laith <lithtartus@gmail.com>
2026-03-13 15:08:42 +0300

    test

--- utils/mail.js
+++ utils/mail.js
@@ -2,16 +2,19 @@ import nodemailer from 'nodemailer';
 
 let transporter = nodemailer.createTransport({
     host:'smtp.gmail.com',
-    post:465,
-    secure:true,
+    post:587,
+    secure:false,
     service:'gmail',
     auth:{
         user: process.env.APP_EMAIL,
         pass: process.env.APP_PASSWORD
     },
+    requireTLS:true,
     tls:{
         rejectUnauthorized:false
-    }
+    },
+    connectionTimeout:30000,
+    socketTimeout:30000
 
 });
 
